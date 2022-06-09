8de107c121c1aa152f539049e75da2afbe840b19
MilonMridha <milonmridha570@gmail.com>
2022-06-09 14:08:10 +0600

    my name added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,9 +13,9 @@ const Hero = () => {
         <div className='flex items-center h-full pt-8'>
           <div className='flex-1 flex flex-col items-center lg:items-start'>
             <p className='text-lg text-accent text-md mb-[22px]'>
-              Hey, I'm Jane! 👋
+              Hey, I'm Milon Mridha! 👋
             </p>
-            <h1 className='text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
+            <h1 className='text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-4xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
               I Build & Design <br /> Web Interfaces.
             </h1>
             <p className='pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left'>
